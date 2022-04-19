09f49330a009e7456fd097459d361aeb5d25d17d
sarthakjdev <jsarthak448@gmail.com>
2022-04-19 16:20:20 +0530

    fix: tailwind config option content chnaged to purge

---
+++ src/components/comingSoon.jsx
@@ -0,0 +1,7 @@
+export default function ComingSoon(){
+    return (
+
+        <h1 className="uppercase text-white text-6xl">Coming soon</h1>
+
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', './sections/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', './sections/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
