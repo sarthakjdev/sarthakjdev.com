bc09353aac9bd1dfd151a63b0eef92c94267ee96
sarthakjdev <jsarthak448@gmail.com>
2022-04-19 23:19:41 +0530

    fix: test

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,6 @@ import '../styles/globals.css'
 import MainLayout from '../src/components/layouts/main.jsx'
 import "nprogress/nprogress.css"
 import TopProgressBar from '../src/configs/topProgressBarConfig'
-// const TopProgressBarFn = dynamic(
-//   () => {
-//     return TopProgressBar
-//   },
-//   { ssr: false },
-// );
-
 
 export default function MyApp({ Component, pageProps }) {
   return (
