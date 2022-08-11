5f2abaa3d69d46d1b6d2aa85a65614c6070e7c6d
sarthakjdev <jsarthak448@gmail.com>
2022-08-11 20:52:31 +0530

    feat: added latest content tweets
    Signed-off-by: sarthakjdev <jsarthak448@gmail.com>

--- pages/tweets.js
+++ pages/tweets.js
@@ -1,32 +1,32 @@
-import Head from 'next/head'
-import TweetsSection from '../src/components/tweets/tweets.jsx'
-import { twitterAxiosClient } from '../src/utils/axiosClient.js'
-import configs from '../src/configs/config.js'
-import threads from '../src/data/recentThreads.js'
+import Head from "next/head";
+import TweetsSection from "../src/components/tweets/tweets.jsx";
+import { twitterAxiosClient } from "../src/utils/axiosClient.js";
+import configs from "../src/configs/config.js";
+import threads from "../src/data/recentThreads.js";
 
-export default function Tweets({tweets}){
-    return(
-            <>
-                <Head>
-                <title>Tweets | Sarthak Jain</title>
-                </Head>
-                <TweetsSection tweets={tweets} />
-            </>
-    )
+export default function Tweets({ tweets }) {
+  return (
+    <>
+      <Head>
+        <title>Tweets | Sarthak Jain</title>
+      </Head>
+      <TweetsSection tweets={tweets} />
+    </>
+  );
 }
 
-export async function getStaticProps(){
-    let threadTweets = []
-    threads.map(async (t)=>{
-        const tweet = await twitterAxiosClient.get(`/tweets/${t.id}`)
-//  console.log("tweet ", tweet.data.data);
-        threadTweets.push(tweet.data.data)
-//  console.log("threadTweets ", threadTweets);
+export async function getStaticProps() {
+    const tweets = [];
+  await Promise.all(
+    await threads.map(async (t) => {
+      const tweet = await twitterAxiosClient.get(`/tweets/${t.id}`);
+      await tweets.push(tweet.data.data);
     })
-    return {
-        props: {
-            tweets: threadTweets
-        },
-        revalidate: 3600
-    }
+  );
+
+  return {
+    props: {
+      tweets: tweets,
+    },
+  };
 }
