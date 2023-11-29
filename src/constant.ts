6c3a2f0417a9421abee69665a6dac322501e618a
sarthakjdev <jsarthak448@gmail.com>
2023-11-29 17:04:12 +0530

    fix: add peerlist in footer
    Signed-off-by: sarthakjdev <jsarthak448@gmail.com>

--- src/constant.ts
+++ src/constant.ts
@@ -3,4 +3,5 @@ export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development'
 
 export const IS_STAGING = !IS_DEVELOPMENT && !IS_PRODUCTION
 
-export const siteDescription = "Specializing in backend engineering and DevOps, I'm dedicated to transforming core ideas into high-performance digital realities.Streamlining development processes and building robust, scalable solutions.Let’s drive innovation together! 🚀"
+export const siteDescription =
+	"Specializing in backend engineering and DevOps, I'm dedicated to transforming core ideas into high-performance digital realities.Streamlining development processes and building robust, scalable solutions.Let’s drive innovation together! 🚀"
